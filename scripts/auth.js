baf72483126c55a9e218c1b91a7c487b38f24a1f
toshiyanishimura1963 <toshiya.nishimura@gmail.com>
2019-01-28 23:09:45 +0900

    lesson 9

---
+++ scripts/auth.js
@@ -0,0 +1,58 @@
+// get data
+db.collection('guides').get().then(snapshot => {
+    setupGuides(snapshot.docs);
+});
+
+// listen for auth status change
+auth.onAuthStateChanged(user => {
+    if (user) {
+        console.log('user login: ', user);
+    } else {
+        console.log('user logout.')
+    }
+});
+
+// signup
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // get user info
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
+
+    // sign up the user
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        // close the signup modal & reset form
+        const modal = document.querySelector('#modal-signup');
+        M.Modal.getInstance(modal).close();
+        signupForm.reset();
+    });
+});
+
+// logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut();
+});
+
+// login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // get user info
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+
+    // sign up the user
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+        console.log(cred.user);
+        // close the login modal & reset form
+        const modal = document.querySelector('#modal-login');
+        M.Modal.getInstance(modal).close();
+        loginForm.reset();
+    });
+});
+
